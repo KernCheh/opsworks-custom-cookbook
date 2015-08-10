14bce471a4151adcc311cec31b277862971ecdf4
Sim Kern Cheh <kern.cheh@gmail.com>
2015-08-10 21:07:07 +0800

    Fix sotong

--- opsworks_custom_env/recipes/configure.rb
+++ opsworks_custom_env/recipes/configure.rb
@@ -1,6 +1,4 @@
-Chef::Log.debug("Current layer is #{node[:opsworks][:instance][:layers]}")
-
-if %w(rails-app sidekiq).include?(node[:opsworks][:instance][:layers])
+if node[:opsworks][:instance][:layers].include?('rails-app') || node[:opsworks][:instance][:layers].include?('sidekiq')
 
   include_recipe "opsworks_custom_env::restart_command"
   include_recipe "opsworks_custom_env::write_config"
