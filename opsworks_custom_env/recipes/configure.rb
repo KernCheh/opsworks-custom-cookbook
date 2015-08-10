f47460941c9c32750e835b712a92926eb470226a
Sim Kern Cheh <kern.cheh@gmail.com>
2015-08-10 20:59:36 +0800

    DEBUG statement

--- opsworks_custom_env/recipes/configure.rb
+++ opsworks_custom_env/recipes/configure.rb
@@ -1,3 +1,5 @@
+Chef::Log.debug("Current layer is #{node[:opsworks][:instance][:layers]}")
+
 if %w(rails-app sidekiq).include?(node[:opsworks][:instance][:layers])
 
   include_recipe "opsworks_custom_env::restart_command"
