c550a8f104ad849a59311f1a836055020620a9d4
Sim Kern Cheh <kern.cheh@gmail.com>
2015-08-12 22:05:52 +0800

    Setup Swap recipe

---
+++ setup_swap/metadata.rb
@@ -0,0 +1,7 @@
+name 'setup_swap'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures setup_swap'
+long_description 'Installs/Configures setup_swap'
+version '0.1.0'
